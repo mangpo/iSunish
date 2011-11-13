037c206847604a6e93de49204710c7e99e9df067
Phitchaya Phothilimthana <mangpo@mit.edu>
2011-11-13 00:39:17 -0500

    working color detect

---
+++ test-app/Classes/HSL.m
@@ -0,0 +1,23 @@
+//
+//  HSL.m
+//  test-app
+//
+//  Created by Phitchaya Phothilimthana on 11/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HSL.h"
+
+
+@implementation HSL
+@synthesize hue,saturation,light;
+-(id) init
+{
+    [super init];
+}
+
+-(void) dealloc 
+{
+    [super dealloc];
+}
+@end
--- test-app/Classes/test_appViewController.h
+++ test-app/Classes/test_appViewController.h
@@ -6,6 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "HSL.h"
 
 @interface test_appViewController : UIViewController  <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 {
@@ -19,6 +20,7 @@
 -(double) getHueFromRed:(unsigned char) red green:(unsigned char) green blue:(unsigned char) blue;
 -(double) getAverageHue:(unsigned char*)pixelBytes row:(int) row col:(int) col;
 -(NSString*) getColorFromHue:(double) hue;
+-(NSString*) getColorFromHSL:(HSL*) hsl;
 
 @end
 
